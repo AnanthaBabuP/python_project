545c2b281b1dbfe81adc5233fd92c74eeb3a295d
AnanthaBabuP <123741046+AnanthaBabuP@users.noreply.github.com>
2024-02-13 16:33:34 +0530

    First Commit branch
    First Commit branch.

---
+++ IfElseExcesise.py
@@ -0,0 +1,5 @@
+income= int(input("Income Is : "))
+if(income>7000):
+    print("Scolarship is available")
+else:
+    print("Not eligible For Scolarship")
---
+++ Question3.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = int(input())
+multi = a*b*c
+addtion = a+b+c
+division = multi/addtion
+print(division)
